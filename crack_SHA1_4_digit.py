7144709ab2b136beec63c4f3aa38edfa99ed3bf4
whiteSkar <sypower07@hotmail.com>
2015-10-30 13:04:13 -0700

    crack sha1 scripts now show current progress

--- crack_SHA1_4_digit.py
+++ crack_SHA1_4_digit.py
@@ -1,5 +1,6 @@
 from itertools import product 
 import hashlib
+import random
 import sys
 
 
@@ -10,15 +11,27 @@ if len(sys.argv) != 2:
 target_hash = sys.argv[1]
 salt = target_hash[:len(target_hash) - 40]
 target_hash = target_hash[len(salt):].lower()
+print("salt: %s hash: %s" % (salt, target_hash))
 
 seed_str = '0123456789'
+print("Using seed str: %s" % seed_str)
+
+count = 0
+total_count = 0
 for tuple in product(seed_str, repeat=4):
     word = ''.join(tuple)
     word = salt + word
+    
     sha = hashlib.sha1() 
     sha.update(word.encode())
     sha_out = sha.hexdigest()
-    print("%s %s" %(word, sha_out))
+   
+    count += 1
+    if count == 100:
+        total_count += count
+        count = 0
+        print("%d hashes compared. Current word: %s" %(total_count, word))
+    
     if (sha_out == target_hash):
         print("Match found: %s %s %s" % (word[len(salt):], target_hash, salt))
         sys.exit()
